cbe33bbf3e8d99f853e872a0ec6229c3be3c5488
Roman English Ahmadu <ahmaduromanenglish@gmail.com>
2023-08-27 20:29:48 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 1372
+
+app.get('/api', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: {
+            name: 'Roman English',
+            lastname: 'ahmadu',
+            like: ['food', 'fo', 'mysql'],
+        }
+    })
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
